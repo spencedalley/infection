72a2d0634e414ba5b7fdb7cbd9c445103a7bf8af
spence dalley <spencedalley@users.noreply.github.com>
2016-09-23 15:26:31 -0700

    Completed subset sum problem and test

--- infection/infection.py
+++ infection/infection.py
@@ -1,38 +1,20 @@
+from itertools import chain, combinations
+
 from .user import *
 
 class InfectionSimulation(): 
 
-    def __init__(self, n): 
-        self.users, self.teacherIds, self.selfLearnerIds = generate_users(n)
-        
+    def __init__(self, users, teacherIds, selfLearnerIds): 
+        # self.users, self.teacherIds, self.selfLearnerIds = generate_users(n)
+        self.users = users.copy()
+        self.teacherIds = list(teacherIds) 
+        self.selfLearnerIds = list(selfLearnerIds)
+        self.globalInfectionSet = set()
+        self.teacherIdsExplored = list()
+        self.position = 0
 
-    def total_infection(self, userId): 
+    def infect(self, userId): 
         """Infect user and user's connections
-
-        Parameters
-        ----------
-        userId : str, the id of the user
-
-        Returns
-        -------
-        infectedSet: set, a set of all infected userIds
-        """
-        infectedSet = set()
-
-        if not self.users.get(userId) or self.users[userId].infected == True: 
-            return infectedSet
-
-        self.users[userId].infected = True # change infection 
-        self.users[userId].siteVersion = 'B' # change site-version 
-        infectedSet.add(userId)
-
-        for id in self.users[userId].connections: 
-            infectedSet = infectedSet | self.total_infection(id)
-
-        return infectedSet
-
-    def limited_infection(self, userId, n, strict): 
-        """Infect user and user's connections limit by less than equal to `n`. If `strict` is True limit by exactly `n`. 
 
         Parameters
         ----------
@@ -40,19 +22,133 @@ class InfectionSimulation():
         n      : int, max number of users that can be infected. 
         strict : bool, if True then algorithm attempts to infect exactly `n` users. 
 
-        Return
-        ------
-        result : bool, returns whether or not infecting userId will result in < n infections
-
+        Returns
+        -------
+        infectedSet: 
         """
-        # look at the teachers that we can infect based on a studentId/teacherId
-        # 
-        frontier = [] # just a list of connected people 
+        # This is the modified infected user set...
+        infectedSet = set()
+
+        if not self.users.get(userId) or self.users[userId].infected == True: 
+            return infectedSet
+        
+        self.users[userId].infected = True # change infection 
+        self.users[userId].siteVersion = 'B' # change site-version 
+        if self.users[userId].role == 'teacher': 
+            self.teacherIdsExplored.append(userId)
+
+        infectedSet.add(userId)
+
+        for id in self.users[userId].connections: 
+            infectedSet = infectedSet | self.infect(id)
+
+        return infectedSet
+
+    def total_infection(self, userId): 
+        self.globalInfectionSet = self.infect(userId)
+        # print(len(self.globalInfectionSet))
+
+    def limited_infection(self, target, strict): 
+        infectionCounts = []
+        frontier = {}
+        position = 0
+
+        for id in self.teacherIds: 
+            self.globalInfectionSet = self.infect(id)
+            frontier[position] = self.teacherIdsExplored
+            if len(self.teacherIdsExplored) > 0: 
+                infectionCounts.append(len(self.globalInfectionSet))
+                position += 1
+            self.teacherIdsExplored = list() # reset list
+
+        for id in self.selfLearnerIds: 
+            frontier[position] = [id] 
+            infectionCounts.append(1)
+            position += 1
+
+        print(frontier)
+        print(infectionCounts)
+
+        subsetSumResult = self.subset_sum(infectionCounts, target, strict)
+        # result = self.extractLocations(subsetSumResult, frontier, infectionCounts)
+
+    def subset_sum(self, nArr, target, strict): 
+        closestSum = 0 
+        result = []
+
+        if min(nArr) > target or target <= 0: 
+            return []
+
+        powerset = chain.from_iterable(combinations(nArr, r) for r in range(len(nArr)+1))
+
+        for item in powerset: 
+            tempSum = sum(item)
+            if tempSum == target: 
+                return sorted(list(item))
+            elif tempSum < target and tempSum > closestSum: 
+                closestSum = tempSum 
+                result = list(item)
+
+        if strict: 
+            return []
+        else: 
+            return sorted(result)
+
+    def powerset(iterable):
+        s = list(iterable)
+        return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
 
 
-    def limited_infection_validation(self): 
-        pass
+
+        
+    # def total_infection(self, userId): 
+    #     """Infect user and user's connections
+
+    #     Parameters
+    #     ----------
+    #     userId : str, the id of the user
+
+    #     Returns
+    #     -------
+    #     infectedSet: set, a set of all infected userIds
+    #     """
+    #     infectedSet = set()
+
+    #     if not self.users.get(userId) or self.users[userId].infected == True: 
+    #         return infectedSet
+
+    #     self.users[userId].infected = True # change infection 
+    #     self.users[userId].siteVersion = 'B' # change site-version 
+    #     infectedSet.add(userId)
+
+    #     for id in self.users[userId].connections: 
+    #         infectedSet = infectedSet | self.total_infection(id)
+
+    #     return infectedSet
+
+    # def limited_infection(self, userId, n, strict): 
+    #     """Infect user and user's connections limit by less than equal to `n`. If `strict` is True limit by exactly `n`. 
+
+    #     Parameters
+    #     ----------
+    #     userId : str, the id of the user
+    #     n      : int, max number of users that can be infected. 
+    #     strict : bool, if True then algorithm attempts to infect exactly `n` users. 
+
+    #     Return
+    #     ------
+    #     result : bool, returns whether or not infecting userId will result in < n infections
+
+    #     """
+    #     # look at the teachers that we can infect based on a studentId/teacherId
+    #     # 
+    #     frontier = [] # just a list of connected people 
+
+
+
+    # def limited_infection_validation(self): 
+    #     pass
 
 
 
--- tests/infection_tests.py
+++ tests/infection_tests.py
@@ -1,18 +1,66 @@
 from nose.tools import * 
 
-from infection import infection, user
+from infection.infection import InfectionSimulation 
+from infection.user import User, generate_users
 
-# users, teacherIds, selfLearnerIds = None
+def init_users(): 
+    USERS = {
+        # Teachers
+        '0': User('0', 'teacher', False, 'A', [str(i) for i in range(6, 11)]+['11']),
+        '1': User('1', 'teacher', False, 'A', [str(i) for i in range(11, 16)]+['2', '16']),
+        '2': User('2', 'teacher', False, 'A', [str(i) for i in range(16, 21)]+['15']),
+        '3': User('3', 'teacher', False, 'A', [str(i) for i in range(21, 25)]+['26']),
+        '4': User('4', 'teacher', False, 'A', [str(i) for i in range(26, 31)]),
+        '5': User('5', 'student', False, 'A', ['31']), # no students
+
+        # Students with teachers
+        '6': User('6', 'student', False, 'A', ['0']),
+        '7': User('7', 'student', False, 'A', ['0']),
+        '8': User('8', 'student', False, 'A', ['0']),
+        '9': User('9', 'student', False, 'A', ['0']),
+        '10': User('10', 'student', False, 'A', ['0']),
+        '11': User('11', 'student', False, 'A', ['1', '0']),
+        '12': User('12', 'student', False, 'A', ['1']),
+        '13': User('13', 'student', False, 'A', ['1']),
+        '14': User('14', 'student', False, 'A', ['1']),
+        '15': User('15', 'student', False, 'A', ['1', '2']),
+        '16': User('16', 'student', False, 'A', ['2']),
+        '17': User('17', 'student', False, 'A', ['2']),
+        '18': User('18', 'student', False, 'A', ['2']),
+        '19': User('19', 'student', False, 'A', ['2']),
+        '20': User('20', 'student', False, 'A', ['2']),
+        '21': User('21', 'student', False, 'A', ['3']),
+        '22': User('22', 'student', False, 'A', ['3']),
+        '23': User('23', 'student', False, 'A', ['3']),
+        '24': User('24', 'student', False, 'A', ['3']),
+        '25': User('25', 'student', False, 'A', ['3']),
+        '26': User('26', 'student', False, 'A', ['4', '3']),
+        '27': User('27', 'student', False, 'A', ['4']),
+        '28': User('28', 'student', False, 'A', ['4']),
+        '29': User('29', 'student', False, 'A', ['4']),
+        '30': User('30', 'student', False, 'A', ['4']),
+        '31': User('31', 'student', False, 'A', ['5']),
+
+        # Self Learners
+        '32': User('32', 'student', False, 'A', []),
+        '33': User('33', 'student', False, 'A', []),
+    }
+
+    TEACHER_IDS = ['0', '1', '2', '3', '4', '5']
+    SELF_LEARNER_IDS = [str(i) for i in range(32, 34)]
+
+    return (USERS, TEACHER_IDS, SELF_LEARNER_IDS)
 
 def setup():
-	pass
+	USERS, TEACHER_IDS, SELF_LEARNER_IDS = init_users()
 
 def teardown():
-	pass
-
+    pass
+	# del USERS, TEACHER_IDS, SELF_LEARNER_IDS
+    
 @with_setup(setup, teardown)
 def test_generate_users():
-    users, teacherIds, selfLearnerIds = user.generate_users(20)
+    users, teacherIds, selfLearnerIds = generate_users(20)
 
     assert_true(len(teacherIds) == 2)
     assert_true(len(users) == 20, "\nActual: {}, Expected: {}".format(len(users), 20))
@@ -20,47 +68,64 @@ def test_generate_users():
     assert_true(len(selfLearnerIds) == 3, "\nActual: {}, Expected: {}".format(len(selfLearnerIds), 3))
 
 @with_setup(setup, teardown)
-def test_infection(): 
-    for i in [30, 50, 100]: 
-        infectionSimulation = infection.InfectionSimulation(i)
-        infectionSet = infectionSimulation.total_infection(infectionSimulation.teacherIds[0])
+def test_infect(): 
+    for i in range(6): 
+        USERS, TEACHER_IDS, SELF_LEARNER_IDS = init_users()
+        infectionSimulation = InfectionSimulation(USERS, TEACHER_IDS, SELF_LEARNER_IDS) 
+        infectionSet = infectionSimulation.infect(infectionSimulation.teacherIds[i])
 
-        for id in infectionSet: 
-            assert_true(infectionSimulation.users[id].siteVersion == 'B')
-            assert_true(infectionSimulation.users[id].infected == True)
+        if i == 0 or i == 1 or i == 2: 
+            expected = 18 
+        elif i == 3 or i == 4: 
+            expected = 11 
+        elif i == 5: 
+            expected = 2
 
-        for id in infectionSimulation.selfLearnerIds: 
-            assert_true(infectionSimulation.users[id].siteVersion == 'A')
-            assert_true(infectionSimulation.users[id].infected == False)
+        assert_true(len(infectionSet) == expected, 'Expected: {}, Actual: {}'.format(expected, len(infectionSet)))
 
-        assert_true(len(infectionSet) == 20, 'Should be 20 infected individuals')
+def test_total_infection(): 
+    for i in range(6): 
+        USERS, TEACHER_IDS, SELF_LEARNER_IDS = init_users()
+        infectionSimulation = InfectionSimulation(USERS, TEACHER_IDS, SELF_LEARNER_IDS) 
+        infectionSimulation.total_infection(infectionSimulation.teacherIds[i])
 
-    for i in [50, 100, 1000]: 
-        infectionSimulation = infection.InfectionSimulation(i)
-        infectionSet = infectionSimulation.total_infection(infectionSimulation.teacherIds[-1])
+        if i == 0 or i == 1 or i == 2: 
+            expected = 18 
+        elif i == 3 or i == 4: 
+            expected = 11 
+        elif i == 5: 
+            expected = 2
 
-        for id in infectionSet: 
-            assert_true(infectionSimulation.users[id].siteVersion == 'B')
-            assert_true(infectionSimulation.users[id].infected == True)
+        assert_true(len(infectionSimulation.globalInfectionSet) == expected, 'Expected: {}, Actual: {}'.format(expected, len(infectionSimulation.globalInfectionSet)))
 
-        for id in infectionSimulation.selfLearnerIds: 
-            assert_true(infectionSimulation.users[id].siteVersion == 'A')
-            assert_true(infectionSimulation.users[id].infected == False)
+def test_limited_infection(): 
+    nSet = [1, 10, 20, 30, 50]
 
-        assert_true(len(infectionSet) == 7, 'Should be 7 infected individuals')
+    for i in nSet: 
+        USERS, TEACHER_IDS, SELF_LEARNER_IDS = init_users()
+        infectionSimulation = InfectionSimulation(USERS, TEACHER_IDS, SELF_LEARNER_IDS) 
+        infectionSimulation.limited_infection(i, False)
+        # assert True == False
 
-        infectionSet = infectionSimulation.total_infection(infectionSimulation.teacherIds[-2])
+def test_subset_sum(): 
+    USERS, TEACHER_IDS, SELF_LEARNER_IDS = init_users()
+    infectionSimulation = InfectionSimulation(USERS, TEACHER_IDS, SELF_LEARNER_IDS) 
 
-        for id in infectionSet: 
-            assert_true(infectionSimulation.users[id].siteVersion == 'B')
-            assert_true(infectionSimulation.users[id].infected == True)
+    testArr = [3, 4, 1, 2]
+    assert_true(infectionSimulation.subset_sum(testArr, 7, False) == [3, 4])
+    assert_true(infectionSimulation.subset_sum(testArr, 8, False) == [1, 3, 4])
+    assert_true(infectionSimulation.subset_sum(testArr, 10, False) == [1, 2, 3, 4])
 
-        for id in infectionSimulation.selfLearnerIds: 
-            assert_true(infectionSimulation.users[id].siteVersion == 'A')
-            assert_true(infectionSimulation.users[id].infected == False)
+    testArr = [3, 4]
+    assert_true(infectionSimulation.subset_sum(testArr, 1, False) == [])
+    assert_true(infectionSimulation.subset_sum(testArr, 7, True) == [3, 4])
+    assert_true(infectionSimulation.subset_sum(testArr, 5, True) == [])
 
-        assert_true(len(infectionSet) == 10, 'Should be 10 infected individuals')
+    testArr = [10, 3, 2]
+    assert_true(infectionSimulation.subset_sum(testArr, 6, False) == [2, 3])
 
+    testArr = [10, 3, 1, 4, 21]
+    assert_true(infectionSimulation.subset_sum(testArr, 9, False) == [1, 3, 4])
 
 
 
